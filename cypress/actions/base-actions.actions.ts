c18b42e4b738a52da50f2be097881d974f6a1746
Mahdi Shokri <mahdi.shokri@specific-group.com>
2023-10-23 21:25:58 +0330

    base actions

--- cypress/actions/base-actions.actions.ts
+++ cypress/actions/base-actions.actions.ts
@@ -1,9 +1,9 @@
 export class baseActions {
     static check_matomo(actual, expected): {} {
-        var keys_to_check = ['fa_ef', 'e_c', 'e_a', 'e_n', 'e_v', 'fa_id', '', 'action_name']
-        var keys_to_not_check = ['dimension11', 'dimension25', 'dimension7', 'dimension21']
-        for (var prop in expected) {
-            if (keys_to_check.includes(prop)) {
+        const keys_to_check = ['fa_ef', 'e_c', 'e_a', 'e_n', 'e_v', 'fa_id', '', 'action_name'];
+        const keys_to_not_check = ['dimension11', 'dimension25', 'dimension7', 'dimension21'];
+        for (const prop in expected) {
+            if (keys_to_check.includes(prop) && !keys_to_not_check.includes(prop) ) {
                 if (actual[prop] !== expected[prop]) {
                     return false
                 }
